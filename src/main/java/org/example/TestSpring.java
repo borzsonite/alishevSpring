9ace3b3aca77c3751de08cf2ae6a16a733c22732
Sergey Rozhkov <borzsonite@gmail.com>
2021-05-05 06:59:13 +0300

    lesson7 end

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -8,6 +8,9 @@ public class TestSpring {
         // Music music =  context.getBean("musicBean", Music.class);
         // MusicPlayer musicPlayer = new MusicPlayer(music);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(musicPlayer == musicPlayer1);
+
         musicPlayer.playSong();
     }
 }
